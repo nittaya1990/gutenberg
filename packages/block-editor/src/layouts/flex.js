d7d147ad8dad1c03c8f5ccf683294a932c52bdd0
ntsekouras <ntsekouras@outlook.com>
2021-09-02 14:19:34 +0300

    [Block Library - Social Links]: Use the new `flex` layout

--- packages/block-editor/src/layouts/flex.js
+++ packages/block-editor/src/layouts/flex.js
@@ -1,7 +1,11 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
+import {
+	__experimentalToggleGroupControl as ToggleGroupControl,
+	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
+} from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -10,14 +14,43 @@ import { appendSelectors } from './utils';
 
 export default {
 	name: 'flex',
-
 	label: __( 'Flex' ),
-
-	edit() {
-		return null;
+	edit: function LayoutFlexEdit( { layout = {}, onChange } ) {
+		const { justifyContent = 'flex-start' } = layout;
+		return (
+			<ToggleGroupControl
+				label={ __( 'Justify content' ) }
+				value={ justifyContent }
+				help={ __( 'Add some help here??' ) }
+				onChange={ ( value ) => {
+					onChange( {
+						...layout,
+						justifyContent: value,
+					} );
+				} }
+				isBlock
+			>
+				<ToggleGroupControlOption
+					value="flex-start"
+					label={ _x( 'Left', 'Justify content option' ) }
+				/>
+				<ToggleGroupControlOption
+					value="center"
+					label={ _x( 'Center', 'Justify content option' ) }
+				/>
+				<ToggleGroupControlOption
+					value="flex-end"
+					label={ _x( 'Right', 'Justify content option' ) }
+				/>
+				<ToggleGroupControlOption
+					value="space-between"
+					label={ _x( 'Space between', 'Justify content option' ) }
+				/>
+			</ToggleGroupControl>
+		);
 	},
-
-	save: function FlexLayoutStyle( { selector } ) {
+	save: function FlexLayoutStyle( { selector, layout } ) {
+		const { justifyContent = 'flex-start' } = layout;
 		return (
 			<style>{ `
 				${ appendSelectors( selector ) } {
@@ -25,6 +58,8 @@ export default {
 					gap: var( --wp--style--block-gap, 0.5em );
 					flex-wrap: wrap;
 					align-items: center;
+					flex-direction: row;
+					justify-content: ${ justifyContent };
 				}
 
 				${ appendSelectors( selector, '> *' ) } {
@@ -33,11 +68,9 @@ export default {
 			` }</style>
 		);
 	},
-
 	getOrientation() {
 		return 'horizontal';
 	},
-
 	getAlignments() {
 		return [];
 	},
